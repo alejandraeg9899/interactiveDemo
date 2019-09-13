855416ff6302b631a46b0f930d9a0387459ba97b
Ale <alejandra_esquivel@enesmorelia.unam.mx>
2019-09-13 08:03:18 -0500

    update

---
+++ App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import { Root } from "native-base";
+
+import * as Font from 'expo-font';
+import { StyleSheet } from 'react-native';
+StyleSheet.setStyleAttributePreprocessor('fontFamily', Font.processFontFamily);
+
+import Home from './screens/home';
+import InputUser from './screens/Input'
+import Login from './screens/login'
+
+class HomeScreen extends React.Component {
+
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
+}
+
+
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: Home,
+  },
+  Login:{
+    screen:Login
+  },
+  Input:{
+    screen:InputUser
+  }
+},{
+    initialRouteName: 'Home',
+  }
+);
+
+export default createAppContainer(AppNavigator);
